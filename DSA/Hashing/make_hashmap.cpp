ae84947b6331b735d788beca3f782f7e78b9147a
Aditya gupta <59690681+adityagupta26@users.noreply.github.com>
2022-10-27 23:01:44 +0530

    Update make_hashmap.cpp
    added delete node

--- DSA/Hashing/make_hashmap.cpp
+++ DSA/Hashing/make_hashmap.cpp
@@ -86,10 +86,23 @@ class Hashtable{
         rehash();
     }
 
-    void erase(string key)
+    void erase(string args)
     {
-
-    }
+    	// Creating an empty HashMap
+		HashMap<String, Integer> hash_map = new HashMap<String, Integer>();
+		// Mapping int values to string keys
+		hash_map.put("Geeks", 10);
+		hash_map.put("4", 15);
+		hash_map.put("Geeks", 20);
+		hash_map.put("Welcomes", 25);
+		hash_map.put("You", 30);
+		// Displaying the HashMap
+		System.out.println("Initial Mappings are: " + hash_map);
+		// Clearing the hash map using clear()
+		hash_map.clear();
+		// Displaying the final HashMap
+		System.out.println("Finally the maps look like this: " + hash_map);
+	}
 
     T search(string key)
     {
